ae088b4a2d864160eb4dca6a85bb1073ae4f1de2
Ppxndpxdd <anawin.pp@gmail.com>
2023-04-25 14:47:58 +0700

    Add project files.

---
+++ KinKanMaiUI/Models/CartDetail.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace KinKanMaiUI.Models
+{
+    [Table("CartDetail")]
+    public class CartDetail
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public int ShoppingCart_Id { get; set; }
+        [Required]
+        public int MenuId { get; set; }
+        [Required]
+        public int Quantity { get; set; }
+        public Menu Menu { get; set; }
+        public ShoppingCart ShoppingCart { get; set; }
+    }
+}
